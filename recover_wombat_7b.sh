476f5c42034598d96bd5ddbabfaa4b0b610e2475
Zheng Yuan <mike29@126.com>
2023-04-13 14:11:21 +0800

    Create recover_wombat_7b.sh

---
+++ recover_wombat_7b.sh
@@ -0,0 +1,9 @@
+python apply_delta.py \
+    --base ./llama-7b \
+    --target ./wombat-7b \
+    --delta GanjinZero/wombat-7b-delta
+
+python apply_delta.py \
+    --base ./llama-7b \
+    --target ./wombat-7b-gpt4 \
+    --delta GanjinZero/wombat-7b-gpt4-delta
